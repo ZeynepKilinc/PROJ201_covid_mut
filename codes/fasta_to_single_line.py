d7193c440fc37732da094f8f3f02f9c20436352f
demircanserdar <126875308+demircanserdar@users.noreply.github.com>
2023-03-26 22:38:39 +0300

    Update and rename proj201.py to fasta_to_single_line.py

--- codes/fasta_to_single_line.py
+++ codes/fasta_to_single_line.py
@@ -7,18 +7,16 @@ def fasta_to_single_line(filename):
     with open(filename) as input_file, open('updated_covid_nucleotide_sequences.fasta', 'w') as new_file: #open the given file and create new in the same statement
         sequence=''  # the string that will be updated and writen into the new file
         for line in input_file:
-            if line.startswith('>'):
+            if line.startswith('>'): #descriptive lines are the ones that start with '>'
                 if sequence!='':   #if sequence variable is updated previously, it is writen first as a whole
-                    new_file.write(sequence + '\n')  #\n character is added because descriptive line (which will be written after this) needs to be seprated from the data                  
+                    new_file.write(sequence + '\n')  #\n character is added because descriptive line (which will be written after this) needs to be seprated from the data
                     sequence='' #sequence is emptied after writing operation
                 new_file.write(line) #desciriptive line is written into the file 
-            else:   
+            else:   #here the program goes over the lines that hold the genome data
                 sequence+=(line.replace('\n', ''))   #sequence variable is is updated to hold the lines of data without \n character in each loop until the descriptive line
 
         if sequence!='': #this statement is for the final data block which does not have a descriptive line after it
             new_file.write(sequence + '\n')
-
-fasta_to_single_line('test.txt')
 
 
 
